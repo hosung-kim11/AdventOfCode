69719480e9722b91f3378f0998b58e6b4adae6e3
Hosung Kim <hk63@rice.edu>
2024-12-28 16:09:24 -0500

    part 2

--- 2024/day2/part2.py
+++ 2024/day2/part2.py
@@ -4,31 +4,35 @@ file_path = os.path.join(os.path.dirname(__file__), "input.txt")
 res = 0
 with open(file_path, "r") as file:
     for line in file:
-        bad = 0
         line = [int(char) for char in line.strip().split()]
-        increase = False
-        prev = line[0]
+        bad = 0
         safe = True
-        if line[1] > prev:
-            increase = True
-        for i in range(1, len(line)):
-            if increase == True:
-                if(line[i] - prev) > 3 or (line[i] - prev) <= 0:
-                    bad += 1
-                    if(bad > 1):
-                        safe = False
-                        break
-                else:
-                    prev = line[i]
-            else:
-                if (prev - line[i]) > 3 or (prev - line[i]) <= 0:
-                    bad += 1
-                    if(bad > 1):
-                        safe = False
-                        break
-                else:
-                    prev = line[i]
-        if safe:
-            res += 1
+        increasing = True
+        prev = line[0]
+        if (prev > line[1]):
+            increasing = False
 
+        for i in range (1, len(line)):
+            diff = line[i] - prev
+            if(increasing):
+                if(diff <= 0 or diff > 3):
+                    bad += 1
+                    if(i == 1 and i < len(line)):
+                        if(line[i] > line[i+1]):
+                            increasing = False
+                    if (bad > 1):
+                        safe  = False
+                        break
+            else:
+                if(diff >= 0 or diff < -3):
+                    bad += 1
+                    if(i == 1 and i < len(line)):
+                        if(line[i] > line[i+1]):
+                            increasing = True
+                    if (bad > 1):
+                        safe = False
+                        break
+            prev = line[i]
+        if(safe):
+            res += 1
 print(res)
